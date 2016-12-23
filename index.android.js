68cac49713bf640a12a32b19b1b725c9654ad62f
Edmond Cheng <edmond.cang@gmail.com>
2016-12-23 18:26:40 +0800

    added drawer

--- index.android.js
+++ index.android.js
@@ -9,31 +9,119 @@ import {
   StyleSheet,
   AppRegistry,
   Text,
+  Image,
   StatusBar,
   Navigator,
   View
 } from 'react-native';
 
-import { Container, Header, Title, Button, Icon } from 'native-base';
+import {
+  Container, Header, Title, Content, Footer, FooterTab, Button, Icon,
+  Card, CardItem, Thumbnail
+} from 'native-base';
+
+import Drawer from 'react-native-drawer';
 
 export default class ReactNativePrototype1 extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      drawerType: 'overlay',
+      openDrawerOffset:100,
+      closedDrawerOffset:0,
+      panOpenMask: .1,
+      panCloseMask: .9,
+      relativeDrag: false,
+      panThreshold: .25,
+      tweenHandlerOn: false,
+      tweenDuration: 350,
+      tweenEasing: 'linear',
+      disabled: false,
+      tweenHandlerPreset: null,
+      acceptDoubleTap: false,
+      acceptTap: false,
+      acceptPan: true,
+      tapToClose: false,
+      negotiatePan: false,
+      rightSide: true,
+    };
+  }
+  closeDrawer = () => {
+    this.drawer.close();
+  };
+  openDrawer = () => {
+    this.drawer.open();
+  };
+  componentDidMount = () => {
+  };
   render() {
     return (
-      <Container>
-        <Header>
-          <Button transparent>
-            <Icon name="ios-arrow-back" />
-          </Button>
 
-          <Title>Header</Title>
+      <Drawer
+        ref={c => this.drawer = c}
+        type="overlay"
+        content={<Text>Hi</Text>}
+        tapToClose={true}
+        openDrawerOffset={0.2} // 20% gap on the right side of drawer
+        panCloseMask={0.2}
+        closedDrawerOffset={-3}
+        styles={drawerStyles}
+        tweenHandler={(ratio) => ({
+          main: { opacity:(2-ratio)/2 }
+        })}
+        >
+        <Container>
 
-          <Button transparent>
-            <Icon name="ios-menu" />
-          </Button>
-        </Header>
-      </Container>
+          <Header>
+            <Button transparent>
+              <Icon name="ios-arrow-back" />
+            </Button>
+
+            <Title>Header</Title>
+
+            <Button transparent onPress={this.openDrawer}>
+              <Icon name="ios-menu" />
+            </Button>
+          </Header>
+
+          <Content>
+            <Card style={{ flex: 0 }}>
+              <CardItem>
+                <Thumbnail source={require('./img/card-image.jpg')} />
+                <Text>NativeBase</Text>
+                <Text note>GeekyAnts</Text>
+              </CardItem>
+
+              <CardItem>
+                <Image style={{ resizeMode: 'cover', width: null }} source={require('./img/card-image.jpg')} />
+              </CardItem>
+
+              <CardItem>
+                <Button transparent>
+                  <Icon name="logo-github" />
+                  1,926
+                </Button>
+              </CardItem>
+           </Card>
+          </Content>
+
+          <Footer>
+            <FooterTab>
+              <Button transparent>
+                <Icon name='ios-call' />
+              </Button>
+            </FooterTab>
+          </Footer>
+
+        </Container>
+      </Drawer>
     );
   }
+}
+
+const drawerStyles = {
+  drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3},
+  main: {paddingLeft: 3},
 }
 
 const titleConfig = {
